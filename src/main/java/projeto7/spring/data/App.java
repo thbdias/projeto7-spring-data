e58d81bb3362e8ff55ca0eb761fb5c70da3ce8c0
thbdias <thiagobalbino91@gmail.com>
2020-01-09 11:11:29 -0300

    create project + readme.md

---
+++ src/main/java/projeto7/spring/data/App.java
@@ -0,0 +1,13 @@
+package projeto7.spring.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
